c461cec62fc88ba1a479ca93e2cdbe678c87cb09
galbi <officialbiash269@gmail.com>
2024-04-01 20:07:47 -0700

    ini commit kedua

--- app.py
+++ app.py
@@ -5,8 +5,19 @@ print()
 def hitungluassegitiga(a,t):
     return round(0,5*a*t,2)
 
-    alas = float(input("input alas segitiga"))
-    tinggi = float(input("tinggi segitiga"))
+alas = float(input("input alas segitiga"))
+tinggi = float(input("tinggi segitiga"))
 
-    print('luas segitiga =',hitungluassegitiga(alas,tinggi))
-    print()
+print('luas segitiga =',hitungluassegitiga(alas,tinggi))
+print()
+
+print("## program python menghitung panjang persegi")
+print("==========================================")
+print()
+
+def hitungpersgipanjang(p,l):
+    return round(p*l,2)
+
+panjang=float(input("input panjang : "))
+lebar=float(input("input lebar : "))
+print('luas persegi panjang =',hitungpersegipanjang(panjang,lebar))
